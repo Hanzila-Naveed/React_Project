9a1e82066402c4e7dfe1b2852201de1bc16e9ac9
Hanzila-Naveed <hanzila.naveed@intagleo.com>
2022-11-03 20:18:22 +0500

    React_project_implementation

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [userName, setUserName] = useState('');
+  useEffect(()=>{
+    getNames();
+  }, [])
+  const getNames = async() => {
+    const response = await axios.get('/names')
+    console.log(response)
+    setUserName(response.data)
+  }
+  return (
+      <>
+      <h1>
+        My frontend
+      </h1>
+        <h3> My name is {userName}</h3>
+
+      </>
+  )
+}
+
+export default App;
